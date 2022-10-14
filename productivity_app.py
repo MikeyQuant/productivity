5a07d6e1b3d8160cc482ed64a3434dfef3229e2b
MikeyQuant <84047197+MikeyQuant@users.noreply.github.com>
2022-10-13 21:04:28 -0400

    Add files via upload

---
+++ productivity_app.py
@@ -0,0 +1,62 @@
+import datetime
+
+from flask import Flask, render_template, request, redirect, url_for, flash,json
+import pandas as pd
+import os
+app = Flask(__name__ )
+BASE="http://192.168.1.151:5000"
+@app.route("/logs",methods=["GET"])
+def logs():
+    df=pd.read_csv("activity_logs.csv", index_col=0).sort_values(by="Datetime", ascending=False)
+    lst=[]
+    for dt,act in zip(df["Datetime"],df["Activity"]):
+       lst.append({"dt":dt,"act":act})
+    return render_template("logs.html",lst=lst,BASE=BASE)
+@app.route("/save_log",methods=["POST"])
+def save_logs():
+    d1=[]
+    df=pd.read_csv("activity_logs.csv",index_col=0)
+    for i in range(len(df)):
+        d2=[request.form[f"{i}d"],request.form[f"{i}"]]
+        print(d2)
+        d1.append(d2)
+    dfn=pd.DataFrame(d1,columns=df.columns,index=[x for x in range(len(d1))])
+    dfn.to_csv("activity_logs.csv")
+    print(dfn)
+    return ""
+
+@app.route("/tasks",methods=["GET"])
+def tasks():
+    df=pd.read_csv("todo.csv", index_col=0).sort_values(by=["Done", "Date_Created"], ascending=[True, True])
+    lst=[]
+    for dt,task,done in zip(df["Date_Created"],df["Task"],df["Done"]):
+       lst.append({"dt":dt,"task":task,"done":done})
+    return render_template("tasks.html",lst=lst,BASE=BASE)
+@app.route("/save_task",methods=["POST"])
+def save_task():
+    d1=[]
+    df=pd.read_csv("todo.csv",index_col=0)
+    for i in range(len(df)):
+        print(request.form)
+        try:
+            if request.form[f"{i}c"]=="on":
+                check="checked"
+            else:
+                check="zzz"
+        except:
+            check="zzz"
+        d2=[request.form[f"{i}"],check,request.form[f"{i}d"]]
+        print(d2)
+        d1.append(d2)
+    dfn=pd.DataFrame(d1,columns=df.columns,index=[x for x in range(len(d1))])
+    dfn.to_csv("todo.csv")
+    print(dfn)
+    return ""
+@app.route("/new_task",methods=["POST"])
+def new_task():
+    task=request.form["task"]
+    df=pd.read_csv("todo.csv",index_col=0)
+    pd.concat([df,pd.DataFrame([[task,"zzz",datetime.datetime.today().strftime("%Y-%m-%d %H:%M")]],columns=df.columns,index=[len(df)])]).to_csv("todo.csv")
+    return redirect(f"{BASE}/tasks")
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
---
+++ productivity_loop.py
@@ -0,0 +1,63 @@
+import smtplib
+import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import os
+import time
+import random
+import pandas as pd
+
+class notify:
+    def __init__(self,base):
+        self.base=base
+    def send_email(self,rec, message,files=[]):
+        msg = MIMEMultipart()
+
+
+
+
+        # Send the message via our own SMTP server.
+
+        #msg.attach(MIMEText(msg, 'plain'))
+        if len(files)>0:
+
+            for file in files:
+                with open(file, 'rb') as f:
+                    img_data = f.read()
+
+
+                image = MIMEImage(img_data, name=os.path.basename(file))
+                msg.attach(image)
+
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login("mikebell180@gmail.com", "felblbqjqihploho")
+        text = msg.as_string()
+        server.sendmail("mikebell180@gmail.com", rec, message)
+        server.quit()
+    def start_text(self):
+        message=f"Productivity Time Starts Now!\nView tasks at {self.base}/tasks"
+        self.send_email("8609298636@txt.att.net",message)
+    def end_text(self):
+        message = f"Productivity Time Ends Now!\nLog your activity at {self.base}/logs"
+        self.send_email("8609298636@txt.att.net", message)
+def main(base):
+    text = notify(base)
+    while 1:
+
+        t = time.localtime()
+        if t.tm_hour==9:
+            minute_interval=random.randrange(0,59)
+            schedule=[x for x in range(9,22,1)]
+            i=0
+            while i<len(schedule)-1:
+                t=time.localtime()
+                if t.tm_hour == schedule[i] and t.tm_min == minute_interval:
+                    df=pd.read_csv("../activity_logs.csv", index_col=0)
+                    pd.concat([df,pd.DataFrame([[datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),"Nothing"]],columns=df.columns,index=[len(df)])]).to_csv("activity_logs.csv")
+                    text.start_text()
+                    time.sleep(360)
+                    text.end_text()
+                    i+=1
+                time.sleep(60)
+            time.sleep(43200)
